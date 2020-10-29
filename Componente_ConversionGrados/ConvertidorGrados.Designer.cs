2b5286b9f6bede88fad344efddc1d067593c2b0f
javiermoreno18 <72530066+javiermoreno18@users.noreply.github.com>
2020-10-28 17:34:43 -0700

    Add files via upload

---
+++ Componente_ConversionGrados/ConvertidorGrados.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace Componente_ConversionGrados
+{
+    partial class ConvertidorGrados
+    {
+        /// <summary> 
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de componentes
+
+        /// <summary> 
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rbCelsius = new System.Windows.Forms.RadioButton();
+            this.rbFarenheit = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnBorrar = new System.Windows.Forms.Button();
+            this.btnConvertir = new System.Windows.Forms.Button();
+            this.tbTotal = new System.Windows.Forms.TextBox();
+            this.tbGrados = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // rbCelsius
+            // 
+            this.rbCelsius.AutoSize = true;
+            this.rbCelsius.Location = new System.Drawing.Point(56, 39);
+            this.rbCelsius.Name = "rbCelsius";
+            this.rbCelsius.Size = new System.Drawing.Size(119, 20);
+            this.rbCelsius.TabIndex = 0;
+            this.rbCelsius.TabStop = true;
+            this.rbCelsius.Text = "Grados celsius ";
+            this.rbCelsius.UseVisualStyleBackColor = true;
+            // 
+            // rbFarenheit
+            // 
+            this.rbFarenheit.AutoSize = true;
+            this.rbFarenheit.Location = new System.Drawing.Point(56, 62);
+            this.rbFarenheit.Name = "rbFarenheit";
+            this.rbFarenheit.Size = new System.Drawing.Size(128, 20);
+            this.rbFarenheit.TabIndex = 1;
+            this.rbFarenheit.TabStop = true;
+            this.rbFarenheit.Text = "Grados farenheit ";
+            this.rbFarenheit.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rbFarenheit);
+            this.groupBox1.Controls.Add(this.rbCelsius);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(56, 40);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(239, 94);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Seleccione la opcion a la que desea convertir los grados ";
+            // 
+            // btnBorrar
+            // 
+            this.btnBorrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBorrar.Location = new System.Drawing.Point(56, 266);
+            this.btnBorrar.Name = "btnBorrar";
+            this.btnBorrar.Size = new System.Drawing.Size(85, 30);
+            this.btnBorrar.TabIndex = 2;
+            this.btnBorrar.Text = "Borrar";
+            this.btnBorrar.UseVisualStyleBackColor = true;
+            this.btnBorrar.Click += new System.EventHandler(this.btnBorrar_Click);
+            // 
+            // btnConvertir
+            // 
+            this.btnConvertir.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConvertir.Location = new System.Drawing.Point(210, 266);
+            this.btnConvertir.Name = "btnConvertir";
+            this.btnConvertir.Size = new System.Drawing.Size(85, 30);
+            this.btnConvertir.TabIndex = 3;
+            this.btnConvertir.Text = "Convertir";
+            this.btnConvertir.UseVisualStyleBackColor = true;
+            this.btnConvertir.Click += new System.EventHandler(this.btnConvertir_Click);
+            // 
+            // tbTotal
+            // 
+            this.tbTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbTotal.Location = new System.Drawing.Point(56, 214);
+            this.tbTotal.Name = "tbTotal";
+            this.tbTotal.Size = new System.Drawing.Size(239, 22);
+            this.tbTotal.TabIndex = 4;
+            // 
+            // tbGrados
+            // 
+            this.tbGrados.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbGrados.Location = new System.Drawing.Point(56, 164);
+            this.tbGrados.Name = "tbGrados";
+            this.tbGrados.Size = new System.Drawing.Size(239, 22);
+            this.tbGrados.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(53, 148);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(242, 16);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Ingrese los grados que desea convertir";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(53, 198);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(248, 16);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Los grados ingresados convertidos son:";
+            // 
+            // UserControl1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbGrados);
+            this.Controls.Add(this.tbTotal);
+            this.Controls.Add(this.btnBorrar);
+            this.Controls.Add(this.btnConvertir);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "UserControl1";
+            this.Size = new System.Drawing.Size(371, 375);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton rbCelsius;
+        private System.Windows.Forms.RadioButton rbFarenheit;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnBorrar;
+        private System.Windows.Forms.Button btnConvertir;
+        private System.Windows.Forms.TextBox tbTotal;
+        private System.Windows.Forms.TextBox tbGrados;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ Componente_ConversionGrados/ConvertidorGrados.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Libreria_Grados;
+
+namespace Componente_ConversionGrados
+{
+    public partial class ConvertidorGrados : UserControl
+    {
+        double gradosC, gradosF;
+        
+        public ConvertidorGrados()
+        {
+            InitializeComponent();
+        }
+
+        public void getGradosCyF()
+        {
+            if (rbCelsius.Checked)
+            {
+                gradosF = Convert.ToDouble(tbGrados.Text);
+                double celsiusConvertido = Grados.conversionFaC(gradosF);
+                tbTotal.Text = celsiusConvertido.ToString();
+            }
+            else
+            {
+                gradosC = Convert.ToDouble(tbGrados.Text);
+                double farenheitConvertido = Grados.conversionCaF(gradosC);
+                tbTotal.Text = farenheitConvertido.ToString();
+            }
+        }
+
+        private void btnBorrar_Click(object sender, EventArgs e)
+        {
+            tbGrados.Text = " ";
+            tbTotal.Text = " ";
+        }
+
+        private void btnConvertir_Click(object sender, EventArgs e)
+        {
+            getGradosCyF();
+        }
+    }
+}
